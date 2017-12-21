b6b34c8b9fa9dd2788201e453057af3386f5099e
Ryan Williams <ryan.williams@projectpeople.com>
2017-12-21 14:58:16 +0000

    We've now got the js fetching content from the back-end and displaying it.

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,17 +2,17 @@ var AppComponent = ng.core.Component({
     selector : 'app',
     template : '<p>The ID is {{greeting.id}}</p><p>The content is {{greeting.content}}</p>'
 }).Class({
-    constructor : function() {
-        this.greeting = {id:'XYZ', content:'Hello World'};
-    }
+    constructor : [ng.http.Http, function(http) {
+        var self = this;
+        self.greeting = {id:'', content:''};
+        http.get("/resource").subscribe(function(response) {
+            self.greeting =response.json()
+        });
+    }]
 });
 
 var AppModule = ng.core.NgModule({
-    imports: [ng.platformBrowser.BrowserModule],
+    imports: [ng.platformBrowser.BrowserModule, ng.http.HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 }).Class({constructor : function(){}})
-
-document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
-});
